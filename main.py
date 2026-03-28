f19d70b14051bd7efc549793d3e94656da78ef95
gigaofpk-afk <gigaofpk@gmail.com>
2026-03-28 09:12:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 # A Part of MegaDL-Bot <https://github.com/AsmSafone/MegaDL-Bot>
 
 import os
+import time
 import asyncio
 from config import Config
 from pyrogram import Client, idle
 
-if __name__ == "__main__" :
+# Fix Heroku time-sync issue
+time.sleep(3)
+
+if __name__ == "__main__":
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
+
     plugins = dict(root="megadl")
+
     app = Client(
         "MegaDL-Bot",
         bot_token=Config.BOT_TOKEN,
@@ -17,6 +23,7 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
+
     app.start()
     print('\n\n>>> MegaDL-Bot Started. Join @AsmSafone!')
     idle()
